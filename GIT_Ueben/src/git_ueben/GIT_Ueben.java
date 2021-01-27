61fdd64577d5f7652310b5b428113f7efc991880
kernia17 <kernia17@htl-kaindorf.at>
2021-01-27 15:36:11 +0100

    Erster Commit

---
+++ GIT_Ueben/src/git_ueben/GIT_Ueben.java
@@ -0,0 +1,13 @@
+package git_ueben;
+
+/**
+ *
+ * @author nicla
+ */
+public class GIT_Ueben 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World from Niclas Kern");
+    }
+}
